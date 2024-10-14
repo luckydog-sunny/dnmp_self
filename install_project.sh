8d962b0461d180e949836830bb8b3c747ed265bc
luckydog-sunny <943891935@qq.com>
2024-10-14 08:39:48 -0700

    Add files via upload

---
+++ install_project.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 检查是否传入了项目名称
+if [ -z "$1" ]; then
+  echo "请传入项目名称"
+  exit 1
+fi
+
+# 项目名称参数
+PROJECT_NAME=$1
+
+# 克隆代码并重命名目录
+git clone https://gitee.com/yxx_brightdawn/admintemplete.git "$PROJECT_NAME"
+
+# 进入刚拉取的项目目录
+cd "$PROJECT_NAME" || exit
+
+# 执行cp .env.example .env
+cp .env.example .env
+
+# 修改.env文件中的APP_NAME和DB_DATABASE
+sed -i "1s/^APP_NAME=.*/APP_NAME=${PROJECT_NAME}/" .env
+sed -i "8s/^DB_DATABASE=.*/DB_DATABASE=${PROJECT_NAME}/" .env
+
+echo "项目已成功克隆并配置，项目名称为: $PROJECT_NAME"
