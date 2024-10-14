760976e28559b60fa7d9fad7e6f49004f61a5079
luckydog-sunny <943891935@qq.com>
2024-10-15 02:19:21 +0800

    Update install_project.sh
    update

--- install_project.sh
+++ install_project.sh
@@ -19,7 +19,22 @@ cd "$PROJECT_NAME" || exit
 cp .env.example .env
 
 # 修改.env文件中的APP_NAME和DB_DATABASE
-sed -i "1s/^APP_NAME=.*/APP_NAME=${PROJECT_NAME}/" .env
-sed -i "8s/^DB_DATABASE=.*/DB_DATABASE=${PROJECT_NAME}/" .env
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i "" "1s/^APP_NAME=.*/APP_NAME=${PROJECT_NAME}/" .env
+  sed -i "" "8s/^DB_DATABASE=.*/DB_DATABASE=${PROJECT_NAME}/" .env
+else
+  sed -i "1s/^APP_NAME=.*/APP_NAME=${PROJECT_NAME}/" .env
+  sed -i "8s/^DB_DATABASE=.*/DB_DATABASE=${PROJECT_NAME}/" .env
+fi
+
+cd web || exit
+
+cp .env.example .env
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i "" "1s/^VITE_APP_TITLE =.*/VITE_APP_TITLE = ${PROJECT_NAME}/" .env
+else
+  sed -i "1s/^VITE_APP_TITLE =.*/VITE_APP_TITLE = ${PROJECT_NAME}/" .env
+fi
 
 echo "项目已成功克隆并配置，项目名称为: $PROJECT_NAME"
